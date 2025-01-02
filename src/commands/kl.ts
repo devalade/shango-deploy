3308f3443608621a6486ca341b4fe5273a7698de
Alade Yessoufou <aladecharaf23@gmail.com>
2025-01-02 22:25:00 +0100

    feat: add kamal alias command

---
+++ src/commands/kl.ts
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer';
+import { Repository } from '../lib/git/repository.js';
+import { executeKamal } from '../util/execute-kamal.js';
+
+export async function kamal(cmd: string[]): Promise<void> {
+  executeKamal(cmd);
+  if (cmd.includes('setup')) {
+    const projectPath = process.cwd();
+    const repository = new Repository(projectPath);
+    const answers = await inquirer.prompt<{ repoName: string; isPrivate: boolean }>([
+      {
+        type: 'input',
+        name: 'repoName',
+        message: 'Enter your repository name:',
+      },
+      {
+        type: 'confirm',
+        name: 'isPrivate',
+        message: 'Is this repository private?',
+      }
+    ]);
+    repository.initializeGithubRepository(answers.repoName, answers.isPrivate);
+  }
+}
